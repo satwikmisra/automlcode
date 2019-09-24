f797159b920207b359cb9aeff14ea15c7b67fa3c
Satwik Misra <stkmsa@gmail.com>
2019-09-23 19:32:08 -0500

    Create header_extractor.py

---
+++ header_extractor.py
@@ -0,0 +1,38 @@
+from tika import parser
+
+filein = parser.from_file('autoMLcode_GD.pdf')
+textrawin=(filein['content']).strip().split('\n')
+rawlinesarray=[]
+for line in textrawin:
+    rawlinesarray.append(line)
+
+numofvar=(rawlinesarray[0].replace('Number of variables: ',''))
+rawinvarnames=(rawlinesarray[1].replace('Input: ','').split(','))
+#period should be removed from end of input
+invardims=[]
+invarnames=[]
+for entry in rawinvarnames:
+    newentry=entry.strip()
+    if 'R' in newentry:
+        splitind=newentry.find('R')
+        rawdim=(newentry[splitind+1:len(newentry)])
+        invardims.append(rawdim.split('×'))
+        invarnames.append(newentry[0:splitind].strip())
+    else:
+        invardims.append(0)
+        invarnames.append(newentry.strip())
+rawoutvarnames=(rawlinesarray[2].replace('Output: ','').split(','))
+outvardims=[]
+outvarnames=[]
+for entry in rawoutvarnames:
+    newentry=entry.strip()
+    if 'R' in newentry:
+        splitind=newentry.find('R')
+        rawdim=(newentry[splitind+1:len(newentry)])
+        outvardims.append(rawdim.split('×'))
+        outvarnames.append(newentry[0:splitind].strip())
+    else:
+        outvardims.append(0)
+        outvarnames.append(newentry.strip())
+#above section extracts both the input and output variable information
+#from a pseudcode header
